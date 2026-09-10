753fcb65eac656acf3cdbddb466573a0d0c677a0
jcf0364 <jcf0364@autuni.ac.nz>
2026-09-10 14:53:28 +1200

    added text fields for the countdown
    testing will begin soon..... after connecting everything

--- Assets/Scripts/GameCountDown.cs
+++ Assets/Scripts/GameCountDown.cs
@@ -5,14 +5,34 @@ using System.Collections;
 public class GameCountDown : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI countdownText;
+ 
+     public static bool InputEnabled { get; private set; } = false;
+ 
     void Start()
     {
         StartCoroutine(StartCountdownRoutine());
     }
-
-    // Update is called once per frame
-    void Update()
+ 
+    private IEnumerator StartCountdownRoutine()
     {
-        
+        InputEnabled = false;
+        countdownText.text = "3";
+        yield return new WaitForSeconds(1f);
+        countdownText.text = "2";
+        yield return new WaitForSeconds(1f);
+        countdownText.text = "1";
+        yield return new WaitForSeconds(1f);
+        countdownText.text = "GO!";
+ 
+        StartGameplay();
+ 
+        yield return new WaitForSeconds(1f);
+ 
+        countdownText.gameObject.SetActive(false);
+    }
+ 
+    private void StartGameplay()
+    {
+        InputEnabled = true;
     }
 }
